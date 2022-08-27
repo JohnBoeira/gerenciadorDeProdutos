f16799ab14d2a02af66488c15cc79178c80a220c
JohnBoeira <johnboeira@gmail.com>
2022-08-27 19:11:57 -0300

    feat (product-list) : Adiciona coluna de avaliação Troca valor da coluna por estrelas

--- src/app/shared/stars.component.ts
+++ src/app/shared/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, SimpleChanges } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 
 @Component({
     selector: 'pm-stars',
@@ -7,7 +7,7 @@ import { Component, OnChanges, SimpleChanges } from "@angular/core";
 })
 
 export class StarsComponent implements OnChanges{
-    rating: number = 4;
+    @Input() rating: number = 0;
     cropWidth: number = 75;
 
     ngOnChanges(): void {
